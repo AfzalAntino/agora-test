27aab003e3d23123234fe69986fc6aa0fdd8d087
unknown <afzal.ahmad@antino.com>
2025-02-12 13:01:23 +0530

    Minor changes in app.tsx

--- App.tsx
+++ App.tsx
@@ -44,7 +44,6 @@ const App = () => {
 
   return (
     <>
-      <Text>Say Hello</Text>
       <NavigationContainer linking={linking} fallback={fallbackComp}>
         <Stack.Navigator
           initialRouteName="Home"
